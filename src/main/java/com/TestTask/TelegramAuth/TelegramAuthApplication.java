5ec3e76a5872e6f0b4f3ef11796029e2aa4155fe
gface <89259112970@mail.ru>
2025-05-26 12:18:14 +0300

    Initial commit

---
+++ src/main/java/com/TestTask/TelegramAuth/TelegramAuthApplication.java
@@ -0,0 +1,13 @@
+package com.TestTask.TelegramAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TestTask/TelegramAuth/TelegramAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TestTask.TelegramAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
